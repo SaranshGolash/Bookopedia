e90914a8e8d9838a89762f0fccc1de087c2b0980
SaranshGolash <129923993+SaranshGolash@users.noreply.github.com>
2025-07-09 18:13:09 +0530

    Made changes to the backend part of the website

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import axios from 'axios';
 import pg from 'pg';
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = 3000;
+const _dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true}));
+
+app.set("views", path.join(_dirname, "views"));
+app.set('view engine', 'ejs');
 
 app.get('/', async (req, res) => {
     try {
@@ -18,4 +24,8 @@ app.get('/', async (req, res) => {
         console.error('Error fetching data:', err);
         res.render('index.ejs', { data: null, error: 'Failed to fetch data' });
     }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
